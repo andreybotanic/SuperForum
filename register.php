810cb55bee459d4bbcd927ad305f3e2ea5eeaee6
delphist2008 <delphist2008@gmail.com>
2013-12-12 01:54:07 +0700

    incorrect username submit checking

--- register.php
+++ register.php
@@ -17,22 +17,26 @@ if (strlen($nick) <= 25)
     if (preg_match('.@.', $email))
         if (preg_match('/(?!^[0-9]*$)(?!^[a-zA-Z!@#$%^&*()_+=<>?]*$)^([a-zA-Z!@#$%^&*()_+=<>?0-9]{6,})$/', $pass))
             if ($_POST['password'] == $_POST['repeat_password']) {
-                $pass = md5($pass . 'thisisthesalt');
-                $result = mysql_query("select count(1) FROM $userstable ");
-                $row = mysql_fetch_array($result);
-                $users_num = $row[0];
-                echo $users_num;
-                $users_num++;
-                $query = "INSERT INTO $userstable VALUES('$users_num','$nick','$pass', '$email', '$date', '$avatar')";
-                mysql_query($query) or die(mysql_error());
-                $ok = true;
-                $_SESSION['user_id'] = $users_num;
-                $_SESSION['user_nick'] = $nick;
-                $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
-                $_SESSION['autorised'] = 1;
-                $_SESSION['numposts'] = 3;
-                unset($_SESSION['incorr']);
-                echo '<meta http-equiv="refresh" content="0;URL=/">';
+                $usr = mysql_query("SELECT * FROM $userstable WHERE nick='$nick'");
+                $usr = mysql_fetch_array($usr)['nick'];
+                if (!isset ($usr)) {
+                    $pass = md5($pass . 'thisisthesalt');
+                    $result = mysql_query("select count(1) FROM $userstable ");
+                    $row = mysql_fetch_array($result);
+                    $users_num = $row[0];
+                    echo $users_num;
+                    $users_num++;
+                    $query = "INSERT INTO $userstable VALUES('$users_num','$nick','$pass', '$email', '$date', '$avatar')";
+                    mysql_query($query) or die(mysql_error());
+                    $ok = true;
+                    $_SESSION['user_id'] = $users_num;
+                    $_SESSION['user_nick'] = $nick;
+                    $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
+                    $_SESSION['autorised'] = 1;
+                    $_SESSION['numposts'] = 3;
+                    unset($_SESSION['incorr']);
+                    echo '<meta http-equiv="refresh" content="0;URL=/">';
+                }
             }
 if (!isset($ok)) {
     echo "you have submitted incorrect data<br>";
